84165d14bfe60369ffd13fe8ebadd567abc0c2a3
Moe <33213781+Moe1207@users.noreply.github.com>
2019-11-18 18:07:08 -0500

    asdf

--- Bootcamp/survey.js
+++ Bootcamp/survey.js
@@ -1,5 +1,8 @@
-var firstName = prompt("what is your first name");
-var lastName = prompt("what is your last name");
+// var firstName = prompt("what is your first name");
+// var lastName = prompt("what is your last name");
 var age = prompt("how old are you");
-console.log("your full name is " + firstName + " " + lastName);
-console.log("you are " + age + " years old");
+
+var ageInDays = age * 365.25;
+
+// console.log("your full name is " + firstName + " " + lastName);
+console.log("you are " + age + " years old, about " + ageInDays + " days old");
