438b54446f5622c431377aae9553ee1a562648b5
Momo <Momo@users.noreply.github.com>
2019-11-22 11:55:18 -0500

    adsfa

---
+++ Bootcamp/domEventDemo.js
@@ -0,0 +1,15 @@
+var button = document.querySelector("button");
+// var body = document.body;
+// var isPurple = false;
+// button.addEventListener("click", function () {
+//     if (isPurple) {
+//         body.style.background = "white";
+//     } else {
+//         body.style.background = "purple";
+//     }
+//     isPurple = !isPurple;
+// })
+
+button.addEventListener("click", function () {
+    document.body.classList.toggle("purple");
+})
