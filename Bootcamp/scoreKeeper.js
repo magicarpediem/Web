ef5e8c802c2b4e4aafc8a332f2dbcd64eee698a3
Momo <Momo@users.noreply.github.com>
2019-11-25 10:14:52 -0500

    a

--- Bootcamp/scoreKeeper.js
+++ Bootcamp/scoreKeeper.js
@@ -1,45 +1,45 @@
-var p1Score = document.getElementById("p1Score");
-var p2Score = document.getElementById("p2Score");
-var p1Button = document.getElementById("p1Button");
-var p2Button = document.getElementById("p2Button");
+var score1 = document.getElementById("p1Score");
+var score2 = document.getElementById("p2Score");
+var button1 = document.getElementById("p1Button");
+var button2 = document.getElementById("p2Button");
 var reset = document.getElementById("reset");
 
-var maxScore = document.getElementById("maxScore");
-var maxScoreInput = document.getElementById("maxScoreInput");
-maxScore.textContent = maxScoreInput.value;
-var maxScoreInt = parseInt(maxScore.textContent);
+var max = document.getElementById("maxScore");
+var maxInput = document.getElementById("maxScoreInput");
+max.textContent = maxInput.value;
+var maxInt = parseInt(max.textContent);
 
 
-var isFinished = false;
+var gameOver = false;
 
-p1Button.addEventListener("click", function () {
-    if (!isFinished) {
-        var score = parseInt(p1Score.textContent);
-        p1Score.textContent = score + 1;
-        if (score + 1 == maxScoreInt) {
-            p1Score.style.color = "green";
-            isFinished = true;
+button1.addEventListener("click", function () {
+    if (!gameOver) {
+        var score = parseInt(score1.textContent);
+        score1.textContent = score + 1;
+        if (score + 1 == maxInt) {
+            score1.style.color = "green";
+            gameOver = true;
         }
     }
 });
-p2Button.addEventListener("click", function () {
-    if (!isFinished) {
-        var score = parseInt(p2Score.textContent);
-        p2Score.textContent = score + 1;
-        if (score + 1 == maxScoreInt) {
-            p1Score.style.color = "green";
-            isFinished = true;
+button2.addEventListener("click", function () {
+    if (!gameOver) {
+        var score = parseInt(score2.textContent);
+        score2.textContent = score + 1;
+        if (score + 1 == maxInt) {
+            score1.style.color = "green";
+            gameOver = true;
         }
     }
 });
 reset.addEventListener("click", function () {
-    p1Score.textContent = "0";
-    p1Score.style.color = "black";
-    p2Score.textContent = "0";
-    p2Score.style.color = "black";
+    score1.textContent = "0";
+    score1.style.color = "black";
+    score2.textContent = "0";
+    score2.style.color = "black";
 
 });
-maxScoreInput.addEventListener("change", function () {
-    maxScore.textContent = maxScoreInput.value;
-    maxScoreInt = maxScoreInput.value;
+maxInput.addEventListener("change", function () {
+    max.textContent = maxInput.value;
+    maxInt = maxInput.value;
 })
