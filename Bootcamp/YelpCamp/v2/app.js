002b98d466bc5d3ce8130e1f6a48daab2bf96e5f
magikarpediem <msyed92@outlook.com>
2020-01-12 19:14:54 -0500

    asdf

--- Bootcamp/YelpCamp/v2/app.js
+++ Bootcamp/YelpCamp/v2/app.js
@@ -1,8 +1,32 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
+
 app.set("view engine", "ejs");
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var campground = mongoose.model("Campground", campgroundSchema);
+
+// campground.create(
+// 	{
+// 		name: "Campground 1",
+// 		image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+// 	}, function(error, camground) {
+// 		if (error) {
+// 			console.log("ERROR WHILE CREATING");
+// 			console.log(error);
+// 		} else {
+// 			console.log("CREATED CAMPGROUND: ");
+// 			console.log(campground);
+// 		}
+// 	});
 
 var campgrounds = [
 	{ name: "Campground 1", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80" },
@@ -21,7 +45,15 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-	res.render("campgrounds", { campgrounds: campgrounds });
+	campground.find({}, function(error, campgrounds) {
+		if (error) {
+			console.log("ERROR WHILE FINDING CAMPGROUND");
+			console.log(error);
+		}
+		else {
+			res.render("campgrounds", { campgrounds: campgrounds });
+		}
+	});
 });
 
 app.post("/campgrounds", function (req, res) {
