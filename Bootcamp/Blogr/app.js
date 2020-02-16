fe7db11f7b7cf77b483fad0025284b589a15d9dd
magikarpediem <msyed92@outlook.com>
2020-02-15 21:31:28 -0500

    asdfasd

--- Bootcamp/Blogr/app.js
+++ Bootcamp/Blogr/app.js
@@ -25,12 +25,46 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
+app.get("/", function (req, res) {
+  res.redirect("/blogs");
+});
+
 //Routes
-app.get("/blogs", function(req, res) {
-  res.render("index");
+app.get("/blogs", function (req, res) {
+  Blog.find({}, function (err, blogs) {
+    if (err) {
+      console.log("ERROR FINDING BLOGS");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  })
+});
+
+app.get("/blogs/new", function (req, res) {
+  res.render("new");
+});
+
+app.post("/blogs", function (req, res) {
+  Blog.create(req.body.blog, function (err, newBlog) {
+    if (err) {
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  })
+});
+
+app.get("/blogs/:id", function (req, res) {
+  Blog.findById(req.params.id, function (err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  })
 });
 
 var port = process.env.PORT | 3000;
-app.listen(port, process.env.IP, function() {
+app.listen(port, process.env.IP, function () {
   console.log("Blogr server started");
 });
